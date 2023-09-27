02144a36cb5b6b20a5836a907a428e0156dabec6
Thiago Barros <thiago.barros@rodobank.com.br>
2023-09-27 18:05:24 -0300

    feat: operador de adição

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n, n2, output;
+
+    printf("Digite um numero para ser somado!\n");
+    scanf("%d", &n);
+    printf("Digite outro numero para ser somado!\n");
+    scanf("%d", &n2);
+    
+    output = n + n2;
+
+    printf("O resultado é %d", output);
+}
